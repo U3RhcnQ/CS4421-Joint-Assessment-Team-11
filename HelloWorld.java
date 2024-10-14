e61e2ab9dbd601e79fac857380a4ebaf46a98b06
Sam Mcloughlin <24403822@studentmail.ul.ie>
2024-10-14 10:54:13 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld {
     // Prints "Hello, World"
     public static void main(String[] args){
         
-        System.out.println("Hello, World !!>>>>>>>");
+        System.out.println("Hello, World !!>>>>>");
 
 }

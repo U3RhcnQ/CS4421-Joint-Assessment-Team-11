302b4d9b01c6bead34cc18196e966f44007a545f
U3RhcnQ <165941707+U3RhcnQ@users.noreply.github.com>
2024-10-14 11:03:25 +0100

    InteliJ Setup
    InteliJ Setup

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 class HelloWorld {
     // Program begins with a call to main
     // Prints "Hello, World"
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         System.out.println("Hello, World !!>>>>>");
 
+    }
 }

a4bee31a2677340d2df3eabb7603405a6c10046e
Sam Mcloughlin <24403822@studentmail.ul.ie>
2024-10-22 14:55:45 +0100

    updated cpu info

--- cpuInfo.java
+++ cpuInfo.java
@@ -6,6 +6,9 @@
 
 public class cpuInfo 
 {
+    static {
+        System.loadLibrary("cpuInfoLib");
+    }
     // Refresh the current values and counters - call this before other methods
     public native void read (int seconds);
     public native void read ();
@@ -42,4 +45,23 @@ public class cpuInfo
     // Return the time in "jiffies" or 1/100ths of a second 
     // that the specified core has been in system mode
     public native int getSystemTime (int core);
+
+    public static void main(String[] args){
+        cpuInfo info = new cpuInfo();
+
+        System.out.println("CPU Model: " + info.getModel());
+        System.out.println("Cores per Socket: " + info.coresPerSocket());
+        System.out.println("Socket Count: " + info.socketCount());
+        System.out.println("L1 Data Cache Size: " + info.l1dCacheSize() + " bytes");
+        System.out.println("L1 Instruction Cache Size: " + info.l1iCacheSize() + " bytes");
+        System.out.println("L2 Cache Size: " + info.l2CacheSize() + " bytes");
+        System.out.println("L3 Cache Size: " + info.l3CacheSize() + " bytes");
+
+        for (int core = 0; core < info.socketCount() * info.coresPerSocket(); core++) {
+            System.out.println("Core " + core + " - User Time: " + info.getUserTime(core));
+            System.out.println("Core " + core + " - System Time: " + info.getSystemTime(core));
+            System.out.println("Core " + core + " - Idle Time: " + info.getIdleTime(core));
+        }
+    }
+
 }

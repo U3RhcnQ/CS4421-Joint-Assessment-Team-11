7063efcb3f3f3460eded97de913621322ffb314b
U3RhcnQ <165941707+U3RhcnQ@users.noreply.github.com>
2024-10-22 20:42:48 +0100

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-class HelloWorld {
-    // Program begins with a call to main
-    // Prints "Hello, World"
-    public static void main(String[] args) {
-
-        System.out.println("Hello, World !!>>>>>");
-
-    }
-}

5972b5e8e1c46bd5817def619879b412590941ce
Sam Mcloughlin <24403822@studentmail.ul.ie>
2024-10-24 15:27:46 +0100

    Sam updated cpu

--- SamMclCPU.java
+++ SamMclCPU.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class SamMclCPU {
     public static void main(String[] args) {
@@ -31,35 +32,61 @@ public class SamMclCPU {
 
 
         // Show sizes of L1,L2 and L3 cache
-        System.out.println("l1d=" + cpu.l1dCacheSize() +
-                ", l1i=" + cpu.l1iCacheSize() +
-                ", l2=" + cpu.l2CacheSize() +
-                ", l3=" + cpu.l3CacheSize());
+        System.out.println("l1d=" + (cpu.l1dCacheSize()*0.001) +
+                ", l1i=" + (cpu.l1iCacheSize()*0.001) +
+                ", l2=" + (cpu.l2CacheSize()*0.000001) +
+                ", l3=" + (cpu.l3CacheSize()*0.000001));
 
 
 
         // Sleep for 1 second and display the idle time percentage for
         // core 1.  This assumes 10Hz so in one second we have 100
         cpu.read(1);
-        System.out.println("core 1 idle=" + cpu.getIdleTime(1) + "%");
-        System.out.println("core 1 active=" + (100 - cpu.getIdleTime(1) + "%"));
 
-        if (cpu.coresPerSocket() == 2){
-            System.out.println("core 2 idle=" + cpu.getIdleTime(2) + "%");
-            System.out.println("core 2 active=" + (100 - cpu.getIdleTime(2) + "%"));
+        for (int i = 1; i <= cpu.coresPerSocket(); i++){
+            System.out.println("core" + i + "idle=" + cpu.getIdleTime(i) + "%");
+            System.out.println("core" + i + "active=" + (100 - cpu.getIdleTime(i) + "%"));
         }
 
-        if (cpu.coresPerSocket() == 3){
-            System.out.println("core 3 idle=" + cpu.getIdleTime(3) + "%");
-            System.out.println("core 3 active=" + (100 - cpu.getIdleTime(3) + "%"));
+        ArrayList<Integer> idleTimeArray = new ArrayList<>();
+        for (int i = 1; i <= cpu.coresPerSocket(); i++){
+            idleTimeArray.add(cpu.getIdleTime(i));
         }
 
-        if (cpu.coresPerSocket() == 4){
-            System.out.println("core 4 idle=" + cpu.getIdleTime(4) + "%");
-            System.out.println("core 4 active=" + (100 - cpu.getIdleTime(4) + "%"));
+        double sum = 0;
+        for (int a = 0; a < cpu.coresPerSocket(); a++) {
+            sum += idleTimeArray.get(a);
+        }
+        double idleTimeAverage = sum / cpu.coresPerSocket();
+
+        ArrayList<Integer> userTimeArray = new ArrayList<>();
+        for (int i = 1; i <= cpu.coresPerSocket(); i++){
+            userTimeArray.add(cpu.getUserTime(i));
         }
 
+        double sum1 = 0;
+        for (int a = 0; a < cpu.coresPerSocket(); a++) {
+            sum1 += userTimeArray.get(a);
+        }
+        double userTimeAverage = sum1 / cpu.coresPerSocket();
 
+        ArrayList<Integer> systemTimeArray = new ArrayList<>();
+        for (int i = 1; i <= cpu.coresPerSocket(); i++){
+            systemTimeArray.add(cpu.getSystemTime(i));
+        }
+
+        double sum2 = 0;
+        for (int a = 0; a < cpu.coresPerSocket(); a++) {
+            sum2 += systemTimeArray.get(a);
+        }
+        double systemTimeAverage = sum2 / cpu.coresPerSocket();
+
+        ArrayList<Integer> utilisationAverage = new ArrayList<>();
+        for (int j = 1; j <= cpu.coresPerSocket(); j++) {
+            double untilisationPercentage = ((cpu.getUserTime(1) + cpu.getSystemTime(j)) / (cpu.getUserTime(j) + cpu.getSystemTime(j) + cpu.getIdleTime(j)));
+            System.out.println("Untilisation percentage for core " + j + " is " + untilisationPercentage + "%");
+            utilisationAverage.add(cpu.getIdleTime(j));
+        }
 
     }
 }

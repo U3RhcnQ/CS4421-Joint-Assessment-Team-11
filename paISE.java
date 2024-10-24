20448d15e8f24684f3b62029b04faf22192612e1
Patrick <patrick_1916@yahoo.com>
2024-10-24 11:06:46 +0100

    update

--- paISE.java
+++ paISE.java
@@ -1,29 +1,9 @@
-public class paISE {
-    public static void main(String[] args) {
-        usbInfo usb = new usbInfo();
-        usb.read();
-        // Maybee change the format of the info
-        // Vendor Information
-        for (int j = 1; j <= usb.deviceCount(i); j++) {
-            System.out.println("Bus "+i+" device "+j+
-                            " has vendor "+String.format("0x%04X", usb.vendorID(i,j))+
 
-                    // Product Information
-            for (int j = 1; j <= usb.deviceCount(i); j++) {
-                System.out.println("Bus "+i+" device "+j+
-                        " and product "+String.format("0x%04X", usb.productID(i,j)));
         public class paISE {
 
-                // Number of USB's
-                System.out.println("\nThis machine has "+
-                        usb.busCount()+" USB buses ");
-
-                // USB buses
-                for (int i = 1; i <= usb.busCount(); i++) {
-                    System.out.println("Bus "+i+" has "+
-                            usb.deviceCount(i)+" devices");
-
-                    // Look-up table yoke
+            public static void main(String[] args) {
+                usbInfo usb = new usbInfo();
+                usb.read(); // Read the USB bus information
 
                 // Display the total number of USB buses
                 System.out.println("\nThis machine has " + usb.busCount() + " USB buses");
@@ -35,10 +15,10 @@ public class paISE {
                     // Iterate through each device on the bus
                     for (int j = 1; j <= usb.deviceCount(i); j++) {
                         // Print the vendor and product information for each device
-                        System.out.println("Bus " + i + " device " + j +
-                                " has vendor " + String.format("0x%04X", usb.vendorID(i, j)) +
-                                " and product " + String.format("0x%04X", usb.productID(i, j)));
-                    }
+                       // System.out.println("Bus " + i + " device " + j +
+                                //" has vendor " + String.format("0x%04X", usb.vendorID(i, j)) +
+                                //" and product " + String.format("0x%04X", usb.productID(i, j)));
+                    } 
                 }
             }
         }

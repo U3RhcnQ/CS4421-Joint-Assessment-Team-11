5a7d3838d403e64c54a6d9ae154157f7863b961b
Patrick <patrick_1916@yahoo.com>
2024-10-24 11:01:05 +0100

    changed code to remove syntax erroors

--- paISE.java
+++ paISE.java
@@ -12,6 +12,7 @@ public class paISE {
             for (int j = 1; j <= usb.deviceCount(i); j++) {
                 System.out.println("Bus "+i+" device "+j+
                         " and product "+String.format("0x%04X", usb.productID(i,j)));
+        public class paISE {
 
                 // Number of USB's
                 System.out.println("\nThis machine has "+
@@ -24,10 +25,20 @@ public class paISE {
 
                     // Look-up table yoke
 
+                // Display the total number of USB buses
+                System.out.println("\nThis machine has " + usb.busCount() + " USB buses");
 
+                // Iterate through each USB bus
+                for (int i = 1; i <= usb.busCount(); i++) {
+                    System.out.println("Bus " + i + " has " + usb.deviceCount(i) + " devices");
+
+                    // Iterate through each device on the bus
+                    for (int j = 1; j <= usb.deviceCount(i); j++) {
+                        // Print the vendor and product information for each device
+                        System.out.println("Bus " + i + " device " + j +
+                                " has vendor " + String.format("0x%04X", usb.vendorID(i, j)) +
+                                " and product " + String.format("0x%04X", usb.productID(i, j)));
+                    }
                 }
             }
-
-
-
         }

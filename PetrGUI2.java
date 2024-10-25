46ad112b2caef5db25897f8449e045ec3818642f
U3RhcnQ <165941707+U3RhcnQ@users.noreply.github.com>
2024-10-25 15:50:03 +0100

    Update PetrGUI2.java

--- PetrGUI2.java
+++ PetrGUI2.java
@@ -1,16 +1,21 @@
-
+// Flatlaf LAF
 import com.formdev.flatlaf.FlatLightLaf;
 import javax.imageio.ImageIO;
+// Swing Libraries
 import javax.swing.JButton;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+//Java Events
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+// Java Charting Library
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -18,16 +23,53 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class PetrGUI2 {
 
     private static void createAndShowGUI() {
-        FlatLightLaf.setup();  //Must be called first of all Swing code as this sets the look and feel to FlatDark.
 
-        // Title
-        final JFrame frame = new JFrame("TaskSys");
+        FlatLightLaf.setup();  //Must be called first of all Swing code as this sets the look and feel to FlatDark.
+        final JFrame frame = new JFrame("TaskSys"); // Title
+
+        // Layout
+        GridBagConstraints CPUChart = new GridBagConstraints();
+        CPUChart.weightx = 0.7;
+        CPUChart.weighty = 0.6;
+        CPUChart.fill = GridBagConstraints.BOTH;
+        CPUChart.gridx = 0;
+        CPUChart.gridy = 0;
+        CPUChart.insets = new Insets(10, 10, 10, 10);
+
+        GridBagConstraints CPURightInfo = new GridBagConstraints();
+        CPURightInfo.weightx = 0.1;
+        CPURightInfo.weighty = 0.6;
+        CPURightInfo.fill = GridBagConstraints.NONE;
+        CPURightInfo.gridx = 1;
+        CPURightInfo.gridy = 0;
+
+        GridBagConstraints CPUBottomInfo = new GridBagConstraints();
+        CPUBottomInfo.weightx = 1.0;
+        CPUBottomInfo.weighty = 0.4;
+        CPUBottomInfo.fill = GridBagConstraints.BOTH;
+        CPUBottomInfo.gridx = 0;
+        CPUBottomInfo.gridy = 1;
+
+        GridBagConstraints TitleConstraints = new GridBagConstraints();
+        TitleConstraints.gridx = 0;
+        TitleConstraints.gridy = 0;
+        TitleConstraints.weightx = 1;
+        TitleConstraints.weighty = 0;
+        TitleConstraints.fill = GridBagConstraints.HORIZONTAL;
+        TitleConstraints.ipadx = 0;
+        TitleConstraints.ipady = 30;
+
+        GridBagConstraints TableConstraints = new GridBagConstraints();
+        TableConstraints.gridx = 0;
+        TableConstraints.gridy = 1;
+        TableConstraints.weightx = 1;
+        TableConstraints.weighty = 1;
+        TableConstraints.fill = GridBagConstraints.BOTH;
 
         // Attempt to load the image and handle exception if not available
         try {
@@ -35,19 +77,15 @@ public class PetrGUI2 {
             Image icon = ImageIO.read(new File("images/icon.png"));
             // Set the icon for the JFrame
             frame.setIconImage(icon);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Create The tabbed Pane
-        JTabbedPane tabbedPane = new JTabbedPane();
+        JTabbedPane tabbedPane = new JTabbedPane(); // Create The tabbed Pane
 
         // Add the First Panel
-        JPanel panel1 = new JPanel(false);
+        JPanel panel1 = new JPanel();
         panel1.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
 
         // Chart Code
         XYSeries series;
@@ -76,47 +114,26 @@ public class PetrGUI2 {
         // Create a panel for the chart
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(200, 200));
+        panel1.add(chartPanel, CPUChart);
 
-        c.weightx = 0.7;
-        c.weighty = 0.6;
-        c.fill = GridBagConstraints.BOTH;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.insets = new Insets(10, 10, 10, 10);
-        panel1.add(chartPanel, c);
-
-
-        cpuInfo cpu = new cpuInfo();
-        cpu.read(0);
         JLabel cpu_right_info_text = new JLabel("<html>" +
-                "<font size=-1>" + cpu.getModel() + "</font><br><br>" +
+                "<font size=-1>" + "</font><br><br>" +
                 "<table cellpadding='5' align='center'>"+
-                "<tr><td><font size=-1> Sockets: </font></td><td align='center'>" +cpu.socketCount() + " </td></tr>"+
-                "<tr><td><font size=-1> Cores: </font></td><td align='center'>" + cpu.coresPerSocket() * cpu.socketCount() + " </td></tr>" +
-                "<tr><td><font size=-1> L1 Data Cache: </font></td><td align='center'>" + cpu.l1iCacheSize() + " </td></tr>" +
-                "<tr><td><font size=-1> L1 Intr Cache: </font></td><td align='center'>" + cpu.l1dCacheSize() + " </td></tr>" +
-                "<tr><td><font size=-1> L1 Cache: </font></td><td align='center'>" + (cpu.l1iCacheSize() + cpu.l1dCacheSize()) + " </td></tr>" +
-                "<tr><td><font size=-1> L2 Cache: </font></td><td align='center'>" + cpu.l2CacheSize() + " </td></tr>" +
-                "<tr><td><font size=-1> L3 Cache: </font></td><td align='center'>" + cpu.l3CacheSize() + " </td></tr>" +
+                "<tr><td><font size=-1> Sockets: </font></td><td align='center'>" + " </td></tr>"+
+                "<tr><td><font size=-1> Cores: </font></td><td align='center'>" +  " </td></tr>" +
+                "<tr><td><font size=-1> L1 Data Cache: </font></td><td align='center'>" +  " </td></tr>" +
+                "<tr><td><font size=-1> L1 Intr Cache: </font></td><td align='center'>" + " </td></tr>" +
+                "<tr><td><font size=-1> L1 Cache: </font></td><td align='center'>" +  " </td></tr>" +
+                "<tr><td><font size=-1> L2 Cache: </font></td><td align='center'>" +  " </td></tr>" +
+                "<tr><td><font size=-1> L3 Cache: </font></td><td align='center'>" +  " </td></tr>" +
                 "" +
                 "</table></html>");
-        cpu_right_info_text.setHorizontalAlignment(JLabel.CENTER);
-        c.weightx = 0.1;
-        c.weighty = 0.6;
-        c.fill = GridBagConstraints.NONE;
-        c.gridx = 1;
-        c.gridy = 0;
-        panel1.add(cpu_right_info_text, c);
+
+        panel1.add(cpu_right_info_text, CPURightInfo);
 
         JLabel cpu_bottom_info_panel = new JLabel("CPU Info");
         cpu_bottom_info_panel.setHorizontalAlignment(JLabel.CENTER);
-        c.weightx = 1.0;
-        c.weighty = 0.4;
-        c.fill = GridBagConstraints.BOTH;
-        c.gridx = 0;
-        c.gridy = 1;
-        panel1.add(cpu_bottom_info_panel, c);
-
+        panel1.add(cpu_bottom_info_panel, CPUBottomInfo);
 
         // Timer to update the chart every second
         Timer timer = new Timer(10, new ActionListener() {
@@ -127,21 +144,8 @@ public class PetrGUI2 {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                cores = cpu.coresPerSocket();
-
-                sum = 0;
-                for (counter = 0; counter <= cores; counter++){
-                    sum += (100 - cpu.getIdleTime(counter));
-                }
-
-                double avaerage = (double) sum / cores;
-                //double avaerage =  sum;
-                //double avaerage = 100 - cpu.getIdleTime(1);
-                series.add(time, avaerage);
-                if (series.getItemCount() > 60) {
-                    series.remove(0); // Remove the first (oldest) entry
-                }
-                cpu.read(1);
+                //cores = cpu.coresPerSocket();
+                //cpu.read(1);
                 time++;
 
             }
@@ -163,12 +167,32 @@ public class PetrGUI2 {
         tabbedPane.addTab("Memory Info", null, panel2,"Memory Info");
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_2);
 
-        JPanel panel3 = new JPanel(false);
-        JLabel filler3 = new JLabel("USB Info");
-        filler3.setHorizontalAlignment(JLabel.CENTER);
-        panel3.setLayout(new GridLayout(1, 1));
-        panel3.add(filler3);
-        tabbedPane.addTab("USB Info", null, panel3,"USB Info");
+
+        JPanel USBPanelWrapper = new JPanel(new BorderLayout());
+        USBPanelWrapper.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+        JPanel USBPanel = new JPanel(new GridBagLayout());
+        JLabel USBTitle = new JLabel("<html><span style='font-size:14px; font-weight:bold;'>USB Info</span> " +
+                "<span style='font-size:11px;'> - Data will automatically refresh every 10 seconds or manually</span></html>");
+        USBPanel.add(USBTitle, TitleConstraints);
+
+        // Data to be displayed in the JTable
+        String[][] USBData = {
+                { "Kundan Kumar Jha", "4031", "CSE", "1", "2", "3", "4" },
+                { "Kundan Kumar Jha", "4031", "CSE", "1", "2", "3", "4"}
+        };
+
+        // Column Names
+        String[] USBColumnNames = { "Bus", "Device", "FFunction", "Vendor ID", "Vendor", "Device ID", "Device" };
+
+        // Initializing the JTable
+        JTable USBTable = new JTable(USBData, USBColumnNames);
+        USBTable.setBounds(30, 40, 200, 300);
+
+        // adding it to JScrollPane
+        JScrollPane USBScrollPane = new JScrollPane(USBTable);
+        USBPanel.add(USBScrollPane, TableConstraints);
+        USBPanelWrapper.add(USBPanel);
+        tabbedPane.addTab("USB Info", null, USBPanelWrapper,"USB Info");
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_3);
 
         JPanel panel4 = new JPanel(false);
@@ -209,10 +233,10 @@ public class PetrGUI2 {
     public static void main(final String[] args) {
 
         SwingUtilities.invokeLater(PetrGUI2::createAndShowGUI);
-        System.loadLibrary("sysinfo");
-        sysInfo info = new sysInfo();
-        cpuInfo cpu = new cpuInfo();
-        cpu.read(0);
+        //System.loadLibrary("sysinfo");
+        //sysInfo info = new sysInfo();
+        //cpuInfo cpu = new cpuInfo();
+        //cpu.read(0);
 
         //showCPU();
     }

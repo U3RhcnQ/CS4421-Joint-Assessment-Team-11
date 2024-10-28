95bdb8e2dc6aedd4b47c2bbdfb776630f6caea07
Sam Mcloughlin <24403822@studentmail.ul.ie>
2024-10-28 14:24:24 +0000

    Updated Disk info

--- SamDiskInfo.java
+++ SamDiskInfo.java
@@ -1,24 +1,47 @@
+import java.util.ArrayList;
+
 public class SamDiskInfo {
     public static void main(String[] args){
         percentUsed();
-        diskInformation();
+        diskBlocks();
+        diskNames();
+        diskBlocksUsed();
     }
     public static void percentUsed(){
         diskInfo disk = new diskInfo();
         disk.read();
+        double[] diskPercentageArray = new double[disk.diskCount()];
         for (int i = 0; i < disk.diskCount(); i++) {
             double percentageDisk = ((double)disk.getUsed(i) / disk.getTotal(i))*100;
-            System.out.println("Percentage of Disk used is: " + percentageDisk + "%");
+            diskPercentageArray[i] = percentageDisk;
         }
+        System.out.println(diskPercentageArray);
     }
-    public static void diskInformation(){
+    public static void diskNames(){
+        ArrayList<String> diskNamesArray = new ArrayList<>();
         diskInfo disk = new diskInfo();
         disk.read();
-        // Iterate through all the disks
         for (int i = 0; i < disk.diskCount(); i++) {
-            System.out.println("disk " + disk.getName(i) + " has " +
-                    disk.getTotal(i) + " blocks, of which " +
-                    disk.getUsed(i) + " are used");
+            diskNamesArray.add(disk.getName(i));
         }
+        System.out.println(diskNamesArray);
+    }
+    public static void diskBlocks() {
+        diskInfo disk = new diskInfo();
+        disk.read();
+        long[] diskBlocksArray = new long[disk.diskCount()];
+        for (int i = 0; i < disk.diskCount(); i++) {
+            diskBlocksArray[i] = disk.getTotal(i);
+        }
+        System.out.println(diskBlocksArray);
+    }
+    public static void diskBlocksUsed() {
+        diskInfo disk = new diskInfo();
+        disk.read();
+        long[] diskBlocksUsedArray = new long[disk.diskCount()];
+        for (int i = 0; i < disk.diskCount(); i++) {
+            diskBlocksUsedArray[i] = disk.getUsed(i);
+        }
+        System.out.println(diskBlocksUsedArray);
     }
 }

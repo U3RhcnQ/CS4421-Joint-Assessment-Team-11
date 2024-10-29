4a50953486fd4e0c2d437eb65d65046cd1fd85ef
Patrick <patrick_1916@yahoo.com>
2024-10-29 14:36:58 +0000

    Stuff

--- pa.java
+++ pa.java
@@ -1,27 +1,37 @@
-// this is what I am working off
+// this is not what I am working off
 public class pa {
     public static void showUSB()
     {
         usbInfo usb = new usbInfo();
         usb.read();
-        System.out.println("\nThis computer has "+
-                usb.busCount()+" USB buses ");
+        System.out.println("\nThis computer has "+ usb.busCount()+" USB buses ");
 
         // Iterate through all of the USB buses
         for (int i = 1; i <= usb.busCount(); i++) {
-            System.out.println("Bus "+i+" has "+
-                    usb.deviceCount(i)+" devices");
+            System.out.println("Bus "+i+" has "+ usb.deviceCount(i)+" devices");
 
             // Iterate through all of the USB devices on the bus
             for (int j = 1; j <= usb.deviceCount(i); j++) {
-                System.out.println("Bus "+i+" device "+j+
-                        " has vendor "+String.format("0x%04X", usb.vendorID(i,j))+
-                        " and product "+String.format("0x%04X", usb.productID(i,j)));
+                System.out.println("Bus "+i+" device "+j+ " has vendor ID "+String.format("0x%04X", usb.vendorID(i,j))+
+                        " and product ID "+String.format("0x%04X", usb.productID(i,j)));
 
             // Lookup table
-                System.out.println("This USB's vendor ID is " + usb.vendorID(i, j) + " the company who makes it is called");
+            for (int k = 1; k <= usb.vendorID(i,j); k++) {
 
-                System.out.println("This USB's product ID is " + usb.productID(i, j) + " the company who makes it is called");
+                System.out.println("The device whose Vendor ID is " + String.format("0x%04X", usb.vendorID(i,j)), "is made by " +   );
+
+
+            for (int l = 1; l <= usb.productID(i,j); l++) {
+
+                System.out.println("The device whose Product ID is " + String.format("0x%04X", usb.productID(i,j)), "is made by " +   );
+            }
+
+
+
+
+
+            }
+
 
             }
         }
